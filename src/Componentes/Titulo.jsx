6959bc802034c59fb056397f10739e272f7c7440
Berni <bernalpas@gmail.com>
2021-11-04 20:34:28 -0300

    Hay que ver el fetch

---
+++ src/Componentes/Titulo.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+const Titulo = () => {
+    return (
+        <div>
+            <h1>Mi App de Comunicación</h1>            
+        </div>
+    )
+}
+
+export default Titulo;
+
+
+
+
+
